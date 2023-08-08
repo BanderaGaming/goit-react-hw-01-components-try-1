5396a7138acd4ea48f1f6478c23d95e4a9c7b777
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-08-08 17:57:39 +0200

    FIX 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from "./profile/profile.jsx";
-import { Statistics } from "./data/data.jsx";
-import { FriendList } from "./friends/friends.jsx";
-import { TransactionHistory } from "./transactions/transactions.jsx";
+import { Profile } from "./Profile/Profile.jsx";
+import { Statistics } from "./Statistics/Statistics.jsx";
+import { FriendList } from "./FriendList/FriendList.jsx";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory.jsx";
 import user from "../data/user.json";
 import data from "../data/data.json";
 import friends from "../data/friends.json"
